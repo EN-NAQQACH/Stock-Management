cb40e0d0b6f8780921fb73159bb1a740f1738b38
ENNAQQACH <mohssineennaqqach0@gmail.com>
2023-04-26 21:31:29 +0100

    Update Dashboard

--- js/Script.js
+++ js/Script.js
@@ -1,11 +1,12 @@
 let sidebar =document.querySelector(".side-bar");
 let sidebarbtn =document.querySelector(".sidebarbtn");
 let easly = document.querySelector(".side-bar .logo ");
+//let navLinks = document.querySelectorAll(".navs-links li a");
 
 sidebarbtn.onclick = function() {
 sidebar.classList.toggle("active");
 if (sidebar.classList.contains("active")) {
-    easly.style.padding = "15px 10px 10px 8px";
+    easly.style.padding = "15px 10px 10px 4px";
     easly.style.fontSize = "1.3rem";
     sidebar.style.transition = "all 0.5s ease-in-out";
   } else {
@@ -14,5 +15,28 @@ if (sidebar.classList.contains("active")) {
     sidebar.style.transition = "none";
   }
 }
+
+/*navLinks.forEach(function(navLink) {
+  navLink.addEventListener("click", function(event) {
+    sidebar.classList.remove("active");
+  });
+});*/
+
+/*const links = document.querySelectorAll('.navs-links div a');
+
+links.forEach(link => {
+  link.addEventListener('click', clickHandler);
+});
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+  
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}*/
 
 
