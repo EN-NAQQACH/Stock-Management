5957f60f3a5d7342929cf532788c3b13401c32c9
ENNAQQACH <mohssineennaqqach0@gmail.com>
2023-05-02 23:40:12 +0100

    Connexion file

---
+++ php/connexion.php
@@ -0,0 +1,18 @@
+<?php
+//Information about database
+$servername = "localhost";
+$database = "gestion_stock";
+$username = "root";
+$password = "";
+
+// Try connexion with database
+try {
+    $connexion = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    // Set the PDO error mode to exception, This line sets the error mode of the PDO object to ERRMODE_EXCEPTION, 
+    //which means that if there is an error during the execution of the PDO, it will throw an exception that can be caught and handled.
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //This line returns the PDO object representing the connection if the connection is successful
+    return $connexion;
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
