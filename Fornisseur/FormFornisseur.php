f87a5e89838b42a147738fe0a580b93d7e76da1d
ENNAQQACH <mohssineennaqqach0@gmail.com>
2023-05-04 23:12:53 +0100

    Create Fornisseur Data

---
+++ Fornisseur/FormFornisseur.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/Dashboard.css">
+    <!--bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+    <title>Document</title>
+</head>
+
+<body>
+    <section class="side-bar" id="navbar">
+        <div class="logo">
+            <span>
+                <h4>Easly</h4>
+            </span>
+        </div>
+        <nav class="nav">
+            <div class="nav-links">
+                <a href="../html/Dashboard.html" class="nav-link" data-page="dashboard">
+                    <i class="bx bxs-dashboard icon-link" id="icons"></i>
+                    <span class="name">Tableau de bord</span>
+                </a>
+                <a href="../php/produits.php" class="nav-link">
+                    <i class="bx bx-package icon-link" id="icons"></i>
+                    <span class="name">Produit</span>
+                </a>
+                <a href="../php/clients.php" class="nav-link">
+                    <i class="bx bx-user icon-link" id="icons"></i>
+                    <span class="name">Clients</span>
+                </a>
+                <a href="order.html" class="nav-link">
+                    <i class="bx bx-receipt icon-link" id="icons"></i>
+                    <span class="name">Commandes</span>
+                </a>
+                <a href="vente.html" class="nav-link">
+                    <i class="bx bx-cart-add icon-link" id="icons"></i>
+                    <span class="name">Vente</span>
+                </a>
+                <a href="fornisseur.html" class="nav-link">
+                    <i class="bx bxs-store icon-link" id="icons"></i>
+                    <span class="name">Fornisseur</span>
+                </a>
+                <a href="#" class="nav-link">
+                    <i class="bx bx-log-out icon-link" id="icons"></i>
+                    <span>Déconnexion</span>
+                </a>
+            </div>
+        </nav>
+    </section>
+    <section class="home-section">
+        <nav>
+            <div class="sidebar-button">
+                <i class="bx bx-menu sidebarBtn"></i>
+                <span class="dashboard">Fornisseur</span>
+            </div>
+        </nav>
+        <div class="home-content" id="contentall" id="form" style="overflow: hidden;">
+            <div style="display: flex;align-items: center;">
+                <div style="font-size: 30px;margin: -35px 20px 0 0;cursor: pointer;"><a href="../php/fornisseur.php"><i class='bx bx-arrow-back'></i></a></div>
+                <div style="font-size: 60px;margin-top: -30px;"><i class='bx bxs-user-circle'></i></div>
+            </div>
+            <form style="margin-top: -30px;overflow: hidden;padding:0 15px 0 0;" method="post" action="../Fornisseur/InsertDataFornisseur.php">
+                <div class="row" style="flex-direction: column;align-items: center;">
+                    <div class="col-md-7">
+                        <div class="form-group" style="margin-top: 20px;">
+                            <label for="formGroupExampleInput" style="margin-bottom:5px;">Nom</label>
+                            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Nom" name="Nom" autocomplete="off">
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="form-group" style="margin-top: 20px;">
+                            <label for="formGroupExampleInput2" style="margin-bottom:5px;">Prénom</label>
+                            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Prénom" name="Prenom" autocomplete="off">
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="form-group" style="margin-top: 20px;">
+                            <label for="formGroupExampleInput3" style="margin-bottom:5px;">Téléphone</label>
+                            <input type="number" class="form-control" id="formGroupExampleInput3" placeholder="ex: 0630254859" name="Telephone" autocomplete="off">
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="form-group" style="margin-top: 20px;">
+                            <label for="formGroupExampleInput4" style="margin-bottom:5px;">Adresse</label>
+                            <input type="Adresse" class="form-control" id="formGroupExampleInput4" placeholder="Adresse" name="Adress" autocomplete="off">
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="form-group" style="margin-top: 20px;">
+                            <button type="submit" class="btn btn-primary" style="margin-top: 1px;padding: 6px 30px;background-color: #12192c;border: #394867;" name="BTNAJOUTER"> Ajouter </button>
+                        </div>
+                    </div>
+
+                </div>
+            </form>
+        </div>
+
+</body>
+
+</html>
+
+<?php
+include '../php/script.php';
+?>
--- php/fornisseur.php
+++ php/fornisseur.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+include '../php/connexion.php';
+include '../Fornisseur/Function.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,6 +14,7 @@
   <link rel="stylesheet" href="../css/Dashboard.css" />
   <!--bootstrap-->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
+  <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
   <title>Fornisseur</title>
 </head>
 
@@ -55,18 +61,40 @@
 
   <section class="home-section">
     <nav>
-      <div class="sidebar-button">
+      <d iv class="sidebar-button">
         <i class="bx bx-menu sidebarBtn"></i>
         <span class="dashboard">Fornisseur</span>
-      </div>
+      </d>
     </nav>
-
-    <div class="home-content">
-
-
-
-      <h4>Liste de clients</h4>
-      <div class="tables">
+    <div class="home-content" id="contentall">
+      <p>
+        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"  style="background-color: green;border: #394867;">
+          Filter
+        </button>
+      </p>
+      <div class="collapse" id="collapseExample" style="max-width: 700px;">
+        <div class="card card-body" >
+          <form >
+            <div class="row" style="margin-right:-55px;">
+              <div class="col-md-3" style="margin:5px 0;">
+                <input type="text" class="form-control" placeholder="nom or id ...">
+              </div>
+              <div class="col-md-3" style="margin:5px 0;">
+                <input type="text" class="form-control" placeholder="date de début" onfocus="(this.type = 'date')" onblur="(this.type = 'text')">
+              </div>
+              <div class="col-md-3" style="margin:5px 0;">
+              <input type="text" class="form-control" placeholder="date de fin" onfocus="(this.type = 'date')" onblur="(this.type = 'text')">
+              </div>
+              <div class="col-md-3" style="margin:5px 0;">
+              <button type="submit" class="btn btn-primary" style="background-color: #12192c;border: #394867;"> recherche </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <h4 style="margin-top: 10px;">Liste de Fornisseurs</h4>
+      <button type="button" class="btn btn-primary" style="background-color:#394867;border: #394867;" data-toggle="modal" id="btnedit" data-target="#fullcontent"><a href="../Fornisseur/FormFornisseur.php" style="text-decoration: none;color: white;">Ajouter</a></button>
+      <div class="tables" style="margin-top: 10px;">
         <table class="table table-hover">
           <thead>
             <tr>
@@ -75,76 +103,39 @@
               <th scope="col">Prénom</th>
               <th scope="col">Téléphone</th>
               <th scope="col">Adresse</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>0645039258</td>
-              <td>agadir</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>0645039258t</td>
-              <td>salé</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry the Bird</td>
-              <td>Thornton</td>
-              <td>0645039258r</td>
-              <td>Lnador</td>
-            </tr>
-            <tr>
-              <th scope="row">4</th>
-              <td>Larry the Bird</td>
-              <td>Thornton</td>
-              <td>0645039258</td>
-              <td>marrakech</td>
-            </tr>
-            <tr>
-              <th scope="row">5</th>
-              <td>Larry the Bird</td>
-              <td>Thornton</td>
-              <td>0645039258</td>
-              <td>safi</td>
-            </tr>
-            <tr>
-              <th scope="row">6</th>
-              <td>Larry the Bird</td>
-              <td>Thornton</td>
-              <td>0645039258</td>
-              <td>beni mellal</td>
-            </tr>
-            <tr>
-              <th scope="row">6</th>
-              <td>Larry the Bird</td>
-              <td>Thornton</td>
-              <td>0645039258</td>
-              <td>beni mellal</td>
-            </tr>
-            <tr>
-              <th scope="row">6</th>
-              <td>Larry the Bird</td>
-              <td>Thornton</td>
-              <td>0645039258</td>
-              <td>beni mellal</td>
-            </tr>
+            <!-- gat data from database-->
+            <?php
+            $fornisseur = gatFornisseur();
+            if (!empty($fornisseur) && is_array($fornisseur)) {
+              foreach ($fornisseur as $key => $value) {
+            ?>
+                <tr>
+                  <th scope="row"><?= $value['ID'] ?></th>
+                  <td><?= $value['Nom'] ?></td> <!-- value from database-->
+                  <td><?= $value['Prenom'] ?></td> <!-- value from database-->
+                  <td><?= $value['Telephone'] ?></td> <!-- value from database-->
+                  <td><?= $value['Address'] ?></td> <!-- value from database-->
+                  <td>
+                    <button type="button" class="btn btn-primary" style="background-color: #697ea9;border: #697ea9;" data-toggle="modal" id="btnedit" data-target="#fullcontent"><a href="../Fornisseur/FormFornisseur.php" style="text-decoration: none;color: white;"><i class='bx bx-pencil'></i></a></button>
+                    <button type="button" class="btn btn-primary" style="background-color: #ff6060;border: #ED2B2A;"><a href="" style="text-decoration: none;color: white;"><i class='bx bx-x-circle'></i></a></button>
+                  </td>
+                </tr>
+            <?php
+              }
+            }
+            ?>
           </tbody>
         </table>
       </div>
+
     </div>
-
-  </section>
-  <script src="../js/Script.js"></script>
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
 </body>
 
 </html>
+<?php
+include '../php/script.php';
+?>
