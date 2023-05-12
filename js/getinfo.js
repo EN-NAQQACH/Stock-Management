c6d278cd71c92c549166b5cd65e624110fc86dc9
ENNAQQACH <mohssineennaqqach0@gmail.com>
2023-05-12 22:31:03 +0100

    Load Product's data

--- js/getinfo.js
+++ js/getinfo.js
@@ -25,3 +25,43 @@ function selectid() {
         });
     }
 }
+
+var jqq = jQuery.noConflict();
+
+function selectidproduct() {
+    var xx = document.getElementById('Product').value;
+    if (xx === '') {
+        document.getElementById("NomField").value = "";
+        document.getElementById("PrixField").value = "";
+        document.getElementById("TotalField").value = "";
+        document.getElementById("QuantiteField").value = "";
+    } else {
+        jqq.ajax({
+            url: '../php/fetch_data_produit.php',
+            method: 'POST',
+            data: {
+                id: xx
+            },
+            dataType: 'JSON',
+            success: function (data) {
+                if (data.Nom && data.prix) {
+                    document.getElementById("NomField").value = data.Nom;
+                    document.getElementById("PrixField").value = data.prix;
+                    calculateTotal();
+                } else {
+                    document.getElementById("NomField").value = "";
+                    document.getElementById("PrixField").value = "";
+                }
+            }
+        });
+    }
+}
+function calculateTotal() {
+    var price = parseFloat(document.getElementById("PrixField").value);
+    var quantity = parseFloat(document.getElementById("QuantiteField").value);
+    if (!isNaN(price) && !isNaN(quantity)) {
+        var total = price * quantity;
+        document.getElementById("TotalField").value = total;
+    }
+}
+
