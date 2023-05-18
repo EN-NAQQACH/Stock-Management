e847811a1e4ee9cf2163a309226cd0e4ee023c71
ENNAQQACH <mohssineennaqqach0@gmail.com>
2023-05-18 18:11:58 +0100

    update

--- Commandes/InsertData.php
+++ Commandes/InsertData.php
@@ -1,19 +1,27 @@
 <?php
+
+use FontLib\Table\Type\post;
+
 include '../php/connexion.php';
 session_start();
 var_dump($_POST);
 // Check if the textfields are not empty
-if (empty($_POST['ID_clinet']) || empty($_POST['date']) || empty($_POST['optionvalue']) || empty($_POST['productId']) || empty($_POST['quantityy'])) {
+if (empty($_POST['ID_clinet']) || empty($_POST['date']) || empty($_POST['optionvalue'])) {
     $_SESSION['status'] = "Veuillez saisir les données";
     $_SESSION['status_code'] = "info";
-} elseif (isset($_POST['ajoutercmd'])) {
+} elseif (isset($_POST['addorder'])) {
     // Get data from textfields
+
+
+
+
+
     $idclient = trim($_POST['ID_clinet']);
     $date = trim($_POST['date']);
     $optionvalue = trim($_POST['optionvalue']);
 
 
-    $majmo3 = count($_POST['productId']);
+
 
     // Check if the data already exists in the database
     $sql = "SELECT * FROM $database.commandes WHERE ID_Client=?";
@@ -33,25 +41,63 @@ if (empty($_POST['ID_clinet']) || empty($_POST['date']) || empty($_POST['optionv
             $optionvalue
         ));
 
-            // Get the ID of the inserted row in commandes table
-            $commandeId = $connexion->lastInsertId();
-            for($i =0 ; $i<$majmo3; $i++){
-                $sql = "INSERT INTO `$database`.`article de commande` (id_commandes, id_article, Quantite, Price, Total) VALUES ('$commandeId', '{$_POST['productId'][$i] }', '{$_POST['quantityy'][$i] }', '{$_POST['price'][$i] }', '{$_POST['totalpr'][$i] }')";
+        // Get the ID of the inserted row in commandes table
+        $commandeId = $connexion->lastInsertId();
+
+        /*$sql2 = "INSERT INTO `$database`.`article de commande` (id_commandes, id_article, Quantite, Price, Total) VALUES ";
+                $rows=[];
+                for($k =0; $k<count($_POST['idar']);$k++){
+                    $id=$_POST["idar"][$k];
+                    $pr=$_POST["prix"][$k];
+                    $qte=$_POST["Qty"][$k];
+                    $tot=$_POST["ttl"][$k];
+                    $rows[]="('{$commandeId}','{$id}','{$pr}','{$qte}','{$tot}')";
+                }
+                $sql2 = implode(",",$rows);
+               $connexion->query($sql2);*/
+
+
+        /*for($i =0 ; $i<count($_POST['idar']); $i++){
+                $sql = "INSERT INTO `$database`.`article de commande` (id_commandes, id_article, Quantite, Price, Total) VALUES ('$commandeId', '{$_POST["idar"][$k] }', '{$_POST["prix"][$k] }', '{$_POST["Qty"][$k] }', '{$_POST["ttl"][$k] }')";
                $connexion->query($sql);
                 
-            }
-            // Insert data into article de commande table
+            }*/
+
+        $sql2 = "INSERT INTO `$database`.`article de commande` (id_commandes, id_article, Quantite, Price, Total) VALUES ";
+        $rows = array();
+        $params = array();
+
+        for ($k = 0; $k < count($_POST['idar']); $k++) {
+            $id = $_POST["idar"][$k];
+            $qte = $_POST["Qty"][$k];
+            $pr = $_POST["prix"][$k];
+            $tot = $_POST["ttl"][$k];
+
+            $rows[] = "(?, ?, ?, ?, ?)";
+            $params[] = $commandeId;
+            $params[] = $id;
+            $params[] = $qte;
+            $params[] = $pr;
+            $params[] = $tot;
+        }
+
+        $sql2 .= implode(", ", $rows);
+        $stmt = $connexion->prepare($sql2);
+        $stmt->execute($params);
 
 
-            if ($req->rowCount() != 0){
+        // Insert data into article de commande table
 
-                $_SESSION['status'] = "Les données ont été insérées avec succès";
-                $_SESSION['status_code'] = "success";
-                $_POST = array(); // Clear the form fields
-            } else {
-                $_SESSION['status'] = "Echec d'insertion des données";
-                $_SESSION['status_code'] = "error";
-            }
+
+        if ($req->rowCount() != 0) {
+
+            $_SESSION['status'] = "Les données ont été insérées avec succès";
+            $_SESSION['status_code'] = "success";
+            $_POST = array(); // Clear the form fields
+        } else {
+            $_SESSION['status'] = "Echec d'insertion des données";
+            $_SESSION['status_code'] = "error";
+        }
     }
 }
 
